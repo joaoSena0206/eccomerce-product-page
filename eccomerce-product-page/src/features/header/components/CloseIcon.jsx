b36470e1e8edde6479faf889a4f7fc9c9050c285
João Sena <joaosena0206@gmail.com>
2025-03-15 20:18:29 -0300

    fix: faz o icone de fechar o menu escalar corretamente

--- eccomerce-product-page/src/features/header/components/CloseIcon.jsx
+++ eccomerce-product-page/src/features/header/components/CloseIcon.jsx
@@ -2,13 +2,14 @@ function CloseIcon({ color = "#69707D", onClick }) {
 	return (
 		<svg
 			onClick={onClick}
-			width="14"
-			height="15"
+			width="20"
+			height="20"
+			viewBox="0 0 14 14"
 			xmlns="http://www.w3.org/2000/svg">
 			<path
 				d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
 				fill={color}
-				fill-rule="evenodd"
+				fillRule="evenodd"
 			/>
 		</svg>
 	);
