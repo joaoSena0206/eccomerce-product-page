5d1d467fdb5082341c8820888a5983e1f61f430c
João Sena <joaosena0206@gmail.com>
2025-03-16 10:33:59 -0300

    feat: remove a animação para que o componente não seja renderizado sem necessidade

--- eccomerce-product-page/src/features/header/components/Backdrop.jsx
+++ eccomerce-product-page/src/features/header/components/Backdrop.jsx
@@ -1,13 +1,10 @@
 function Backdrop({ isOpen, onClick }) {
 	return (
-		<div
-			onClick={onClick}
-			className={`fixed left-0 top-0 w-screen h-screen bg-black z-20 transition-opacity duration-300 ease-in-out
-				${
-					isOpen
-						? "opacity-50 pointer-events-auto"
-						: "opacity-0 pointer-events-none"
-				}`}></div>
+		isOpen && (
+			<div
+				onClick={onClick}
+				className={`fixed left-0 top-0 w-screen h-screen bg-black z-20 opacity-50`}></div>
+		)
 	);
 }
 
--- eccomerce-product-page/src/features/header/components/SidebarMenu.jsx
+++ eccomerce-product-page/src/features/header/components/SidebarMenu.jsx
@@ -2,22 +2,22 @@ import CloseIcon from "./CloseIcon";
 
 function SidebarMenu({ isOpen, onClick }) {
 	return (
-		<div
-			className={`fixed top-0 left-0 h-screen w-64 bg-white z-30 p-7 pr-30 shadow-md
-				transform transition-transform duration-300 ease-in-out
-				${isOpen ? "translate-x-0" : "-translate-x-full"}`}>
-			<CloseIcon onClick={onClick} />
+		isOpen && (
+			<div
+				className={`fixed top-0 left-0 h-screen w-64 bg-white z-30 p-7 pr-30 shadow-md`}>
+				<CloseIcon onClick={onClick} />
 
-			<nav className="mt-15">
-				<ul className="font-bold text-[18px] space-y-5">
-					<li>Collections</li>
-					<li>Men</li>
-					<li>Women</li>
-					<li>About</li>
-					<li>Contact</li>
-				</ul>
-			</nav>
-		</div>
+				<nav className="mt-15">
+					<ul className="font-bold text-[18px] space-y-5">
+						<li>Collections</li>
+						<li>Men</li>
+						<li>Women</li>
+						<li>About</li>
+						<li>Contact</li>
+					</ul>
+				</nav>
+			</div>
+		)
 	);
 }
 
